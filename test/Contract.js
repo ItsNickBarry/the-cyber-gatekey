57dce9cb988e38a9900a7bf03e422030c21460e5
Nick Barry <itsnickbarry@protonmail.ch>
2021-04-20 13:48:21 -0500

    improve test output

--- test/Contract.js
+++ test/Contract.js
@@ -1,3 +1,5 @@
+const { expect } = require('chai');
+
 const codes = require('../data/codes.js');
 const hashes = new Set(require('../data/hashes.js'));
 
@@ -8,6 +10,8 @@ describe('Contract', function () {
     const gatekeeper = await ethers.getContractAt('theCyberGatekeeperTwo', '0xbB902569a997D657e8D10B82Ce0ec5A5983C8c7C', signer);
 
     const validCodes = codes.filter(c => hashes.has(ethers.utils.solidityKeccak256(['string'], [c])));
+
+    let pass = false;
 
     for (let passcode of validCodes) {
       const hash = ethers.utils.solidityKeccak256(['string'], [passcode]);
@@ -20,11 +24,16 @@ describe('Contract', function () {
         const interactorFactory = await ethers.getContractFactory('Contract', signer);
         const interactor = await interactorFactory.deploy(ethers.utils.toUtf8Bytes(passcode));
         await interactor.deployed();
-        console.log(passcode);
+        console.log('='.repeat(80));
+        console.log(`Passcode verified: ${ passcode }`);
+        console.log('='.repeat(80));
+        pass = true;
         break;
       } catch (e) {
-        console.log(e);
+        console.log(`Passcode failed: ${ passcode }`);
       }
     }
+
+    expect(pass).to.be.true;
   });
 });
